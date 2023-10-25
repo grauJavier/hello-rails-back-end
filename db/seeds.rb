4d5e01134c86f22dca2e17a7317c162850676567
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-25 20:48:54 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# frozen_string_literal: true
+
+Message.create(greetings: 'Bonjour à tous !!') # French
+Message.create(greetings: 'Buen día a todos!!') # Spanish
+Message.create(greetings: 'Buongiorno a tutti!!') # Italian
+Message.create(greetings: 'Goedemorgen allemaal!!') # Dutch
+Message.create(greetings: 'Bom dia a todos!!') # Portuguese
+Message.create(greetings: 'こんにちは皆さん！！') # Japanese
+Message.create(greetings: 'صباح الخير للجميع!!') # Arabic
+Message.create(greetings: 'Добро јутро свима!!') # Serbian
+Message.create(greetings: 'God morgon allihopa!!') # Swedish
+Message.create(greetings: 'Aloha a me ka laulā!!') # Hawaiian
+Message.create(greetings: 'Hyvää huomenta kaikille!!') # Finnish
+Message.create(greetings: 'God morgen alle sammen!!') # Norwegian
+Message.create(greetings: 'Guten Morgen alle zusammen!!') # German
+Message.create(greetings: 'Добро јутро свима!!') # Serbian (Cyrillic)
+Message.create(greetings: 'Dzień dobry wszystkim!!') # Polish
+Message.create(greetings: 'God morgen alle sammen!!') # Norwegian
+Message.create(greetings: 'Καλημέρα σε όλους!!') # Greek
+Message.create(greetings: '안녕하세요 모두에게!!') # Korean
+Message.create(greetings: 'בוקר טוב לכולם!!') # Hebrew
+Message.create(greetings: 'Günaydın herkese!!') # Turkish
